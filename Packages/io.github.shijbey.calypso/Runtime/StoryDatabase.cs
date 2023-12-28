b928cc55159da40e402d54794efc1500acb6c74a
Shi Johnson-Bey <shijbey@gmail.com>
2023-12-28 10:38:20 -0500

    Add DatabaseViewer editor window

--- Packages/io.github.shijbey.calypso/Runtime/StoryDatabase.cs
+++ Packages/io.github.shijbey.calypso/Runtime/StoryDatabase.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
 using Calypso.RePraxis;
 using UnityEngine;
 
@@ -9,15 +7,9 @@ namespace Calypso
     /// <summary>
     /// Wraps a RePraxis database to make it available to Unity Scripts
     /// </summary>
-    public class StoryDatabase : MonoBehaviour, ISerializationCallbackReceiver
+    public class StoryDatabase : MonoBehaviour
     {
         #region Protected Fields
-
-        /// <summary>
-        /// Unity serializable node representation of the database
-        /// </summary>
-        [SerializeField]
-        private List<string> _serializedSentences = new List<string>();
 
         /// <summary>
         /// Reference to the fully instantiated database
@@ -27,46 +19,5 @@ namespace Calypso
         #endregion
 
         public RePraxisDatabase DB => db;
-
-        #region Serialization Callbacks
-
-        void ISerializationCallbackReceiver.OnAfterDeserialize()
-        {
-            // Do Nothing
-        }
-
-        void ISerializationCallbackReceiver.OnBeforeSerialize()
-        {
-            // Convert the tree representation of the database into a series of string sentences
-
-            // Perform DFS, save the current path, when we hit a leaf, save the path to a string
-            // in the collection
-            _serializedSentences.Clear();
-
-            var nodeStack = new Stack<IRePraxisNode>(DB.Root.Children);
-
-            while (nodeStack.Count > 0)
-            {
-                IRePraxisNode node = nodeStack.Pop();
-
-                IEnumerable<IRePraxisNode> children = node.Children;
-
-                if (children.Count() > 0)
-                {
-                    // Add children to the stack
-                    foreach (var child in children)
-                    {
-                        nodeStack.Push(child);
-                    }
-                }
-                else
-                {
-                    // This is a leaf
-                    _serializedSentences.Add(node.GetPath());
-                }
-            }
-        }
-
-        #endregion
     }
 }
